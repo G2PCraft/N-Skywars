c8c2629d45d45fd727190de52a33a68a03f90114
Unity41 <nascentnova@hotmail.com>
2017-09-10 09:08:31 +0800

    Messages system supports multi-locales. In test progress getPlayerLocale always returns en-US

---
+++ src/com/nekota/mc/skywars/Util/Message.java
@@ -0,0 +1,13 @@
+package com.nekota.mc.skywars.Util;
+
+import org.bukkit.entity.Player;
+
+public class Message {
+	public static void sendMessage(Player player, String messagePath) {
+		
+	}
+	
+	public static String getPlayerLocale(Player player) {
+		return "en-US";
+	}
+}
